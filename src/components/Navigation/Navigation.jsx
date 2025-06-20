f30f846ca69d61f4032d69f0afc32c0659d2e760
Анна <anna.kuvakina91@gmail.com>
2025-06-20 17:50:31 +0300

    Update Navigation.jsx

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,32 +1,12 @@
-import { Link, useLocation, useNavigate } from 'react-router-dom';
-import { useCallback } from 'react';
+import { Link } from 'react-router-dom';
 import s from './Navigation.module.css';
 
 const Navigation = () => {
-
-  const location = useLocation();
-  const navigate = useNavigate();
-
-  const handleAboutClick = useCallback(
-    (e) => {
-      e.preventDefault();
-      if (location.pathname === '/') {
-        const section = document.getElementById('about');
-        if (section) {
-          section.scrollIntoView({ behavior: 'smooth' });
-        }
-      } else {
-        navigate('/#about'); // переходимо на головну з хешем
-      }
-    },
-    [location, navigate]
-  );
-
   return (
     <nav className={s.nav}>
       <ul className={s.nav_list}>
         <li>
-        <a href="#about" onClick={handleAboutClick}>Про фонд</a>
+        <Link to="/">Головна</Link>
         </li>
         <li>
         <Link to="/help">Отримати допомогу</Link>
